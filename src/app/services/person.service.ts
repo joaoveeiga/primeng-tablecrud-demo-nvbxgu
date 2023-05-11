7649c3102655df053ab36644921d837f38cefb5c
joaoveeiga <joaoveeiga@users.noreply.github.com>
2023-05-11 20:08:47 -0300

    Initial commit

---
+++ src/app/services/person.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Person } from '../models/person.model';
+
+@Injectable()
+export class PersonService {
+  constructor(private http: HttpClient) {}
+  getPersons() {
+    return this.http
+      .get<any>('assets/persons.json')
+      .toPromise()
+      .then((res) => <Person[]>res.data)
+      .then((data) => {
+        return data;
+      });
+  }
+}
